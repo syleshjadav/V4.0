d951b189e1abce563b6365e1b265202011698b71
sjadav <sjadav@VM3-A034847>
2017-03-07 16:44:42 -0500

    sdf

---
+++ MyShopCompInspectionSync/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace MyShopCompInspectionSync
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblCountDown = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblCountDown
+            // 
+            this.lblCountDown.AutoSize = true;
+            this.lblCountDown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblCountDown.ForeColor = System.Drawing.Color.IndianRed;
+            this.lblCountDown.Location = new System.Drawing.Point(12, 9);
+            this.lblCountDown.Name = "lblCountDown";
+            this.lblCountDown.Size = new System.Drawing.Size(0, 13);
+            this.lblCountDown.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1244, 720);
+            this.Controls.Add(this.lblCountDown);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Enter += new System.EventHandler(this.Form1_Enter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCountDown;
+        public System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MyShopCompInspectionSync/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyShopCompInspectionSync
+{
+    public partial class Form1 : Form
+    {
+       // private System.Windows.Forms.Timer timer1;
+        private int counter = 60;
+        private int servicePollInterval;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // timer1.Tick += new EventHandler(timer1_Tick);
+
+            //timer1.Interval
+            servicePollInterval = MyShopCompInspectionSync.Properties.Settings.Default.servicePollInterval;
+            counter = servicePollInterval;
+          //  timer1.Start();
+            lblCountDown.Text = counter.ToString();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter--;
+            lblCountDown.Text = counter.ToString();
+            if (counter == 0)
+            {
+                // timer1.Stop();
+
+                counter = servicePollInterval;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+    
+
+        private void Form1_Enter(object sender, EventArgs e)
+        {
+           
+           
+        }
+    }
+}
