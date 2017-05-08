283010eefa43761fcd1775b3d78b60eb1eb9bfb8
sjadav <sjadav@VM3-A034847>
2017-05-08 16:47:49 -0400

    sdf

---
+++ MyShopWebFrontFacing/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("MyShopWebFrontFacing")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("Treasury BITS")>
+<Assembly: AssemblyProduct("MyShopWebFrontFacing")>
+<Assembly: AssemblyCopyright("Copyright © Treasury BITS 2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("8615e1a9-313e-44e6-8a76-a763d700769c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
